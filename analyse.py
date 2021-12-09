b0d96df9c756b990db6fb6829f04da56e89b66af
HellVal <91735409+HellVal@users.noreply.github.com>
2021-12-09 10:12:28 +0100

    Add files via upload

---
+++ analyse.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  9 09:05:49 2021
+
+@author: valerie.hellmueller
+"""
+
+
+# Data analyse
+
+def show_nan(picture):
+    
+    index = np.argwhere(np.isnan(picture)
+                        
+    return index
+
+
+nan_index = show_nan(xxx)
+number_of_nan = np.size(nan_index)
+
+
+b = np.eye(data_shape)
+b = (nan_index)[0]
+
+imshow(a)
+plt.show()
+
+
+
+def show_zero(picture):
+    
+    index = np.where(picture == 0)[0]
+    
+    return index
+
+
+zeros_index = show_zero(XXX)
+number_of_zeros = np.size(zeros_index)
+
+# plot
+
+b = np.eye(data_shape)
+b = (zero_index)[0]
+
+
+imshow(b)
+plt.show()
