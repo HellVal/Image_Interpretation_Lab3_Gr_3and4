734e381c7beaa802fe09963c78adf192890fdb1b
yatao25 <84437331+yatao25@users.noreply.github.com>
2021-12-12 23:24:16 +0800

    Add files via upload

---
+++ temp_classify_trial/temp_classify_train_test.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 1 12:29:33 2021
+@author: Yatao
+@purpose: classification for time-series data
+"""
+import os
+import h5py
+import time
+import psutil
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.svm import LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
+from warnings import filterwarnings
+filterwarnings('ignore')
+
+
+def logistic_classifier(_train_data_vector, _train_gt_vector, _test_data_vector):
+    _lr = LogisticRegression()
+    _lr.fit(_train_data_vector, _train_gt_vector)
+    return _lr.predict(_test_data_vector)
+
+
+def stacking_vector(_data, _gt):
+    # reshape ground truth
+    _gt_vector = []
+    for i_num in range(len(_gt)):
+        _one_gt = np.full(shape=_data.shape[1], fill_value=_gt[i_num], dtype=int)
+        _gt_vector = np.concatenate([_gt_vector, _one_gt])
+    _gt_vector = np.array(_gt_vector)
+    # reshape data
+    _data_vector = []
+    for i_slice in _data:
+        for j_slice in i_slice:
+            _data_vector.append(j_slice.ravel())
+    _data_vector = np.array(_data_vector)
+
+    # exclude pixels that have nan value
+    indices = np.array(np.where(np.all(~np.isnan(_data_vector), axis=1)))
+    _data_vector_r = _data_vector[indices[0], :]
+    _gt_vector_r = _gt_vector[indices[0]]
+    return _data_vector_r, _gt_vector_r
+
+
+def stacking_vector_test(_data, _gt):
+    # reshape ground truth
+    _gt_vector = np.array(_gt).astype(int)
+    # reshape data
+    _data_vector = []
+    for j_slice in _data:
+        _data_vector.append(j_slice.ravel())
+    _data_vector = np.array(_data_vector)
+
+    # exclude pixels that have nan value
+    indices = np.array(np.where(np.all(~np.isnan(_data_vector), axis=1)))
+    _data_vector_r = _data_vector[indices[0], :]
+    _gt_vector_r = _gt_vector[indices[0]]
+    return _data_vector_r, _gt_vector_r
+
+
+def time_series_stacking_classify(_train_data, _train_gt, _test_data, _test_gt):
+    print("\nAlgorithm: time-series classification by stacking per-pixel features into a vector...")
+
+    # data size: class_number * pixel_number * temporal_length * feature_length
+    # ==> (class_number * pixel_number) * (temporal_length*feature_length)
+    _train_data_vector, _train_gt_vector = stacking_vector(_train_data, _train_gt)
+    _test_data_vector, _test_gt_vector = stacking_vector_test(_test_data, _test_gt)
+    print("Train data size: {}, {}, {}, {}".format(_train_data.shape, _train_gt.shape,
+                                                   _train_data_vector.shape, _train_gt_vector.shape))
+    print("test data size: {}, {}, {}, {}".format(_test_data.shape, _test_gt.shape,
+                                                  _test_data_vector.shape, _test_gt_vector.shape))
+
+    # classification
+    classifier_name = ['Logistics regression']
+    classifier_func = [logistic_classifier]
+    for i in range(len(classifier_name)):
+        print("\n{}:".format(classifier_name[i]))
+        _begin_time = time.time()
+        _predict_test_label = classifier_func[i](_train_data_vector, _train_gt_vector, _test_data_vector)
+        print("accuracy: {}\nprecision: {}\nrecall: {}\nf1 Score: {}".format(
+            accuracy_score(_predict_test_label, _test_gt_vector),
+            precision_score(_predict_test_label, _test_gt_vector, average=None),
+            recall_score(_predict_test_label, _test_gt_vector, average=None),
+            f1_score(_predict_test_label, _test_gt_vector, average=None)))
+        print('memory: %.4f GB' % (psutil.Process(os.getpid()).memory_info().rss / 1024 / 1024 / 1024))
+        print('time: {} s'.format(time.time() - _begin_time))
+
+
+if __name__ == '__main__':
+    # root = r"C:\ETH Course\Image interpretation\time_series\lab_3\\"
+    root = "/home/yatao/time_series/lab_3"
+    if os.path.exists(root):
+        print("path exists...")
+    # <KeysViewHDF5 ['DATA', 'GT']>
+    train_h5 = h5py.File(root + '/training_40000.hdf5', 'r')
+    train_data, train_GT = np.array(train_h5['DATA']), np.array(train_h5['GT'])
+    # print("Train data size: {}, {}".format(train_data.shape, train_GT.shape))
+    test_h5 = h5py.File(root + '/test_data.hdf5', 'r')
+    test_data, test_GT = np.array(test_h5['DATA']), np.array(test_h5['GT'])
+    # print("test data size: {}, {}".format(test_data.shape, test_GT.shape))
+    print(np.array(train_GT))
+
+    print('before inference, memory: %.4f GB' % (psutil.Process(os.getpid()).memory_info().rss / 1024 / 1024 / 1024))
+    begin_time = time.time()
+
+    # data size: class_number * pixel_number * temporal_length * feature_length
+    time_series_stacking_classify(train_data, train_GT, test_data, test_GT)
+    print('stacking inference, memory: %.4f GB' % (psutil.Process(os.getpid()).memory_info().rss / 1024 / 1024 / 1024))
+    print('stacking inference time: {}'.format(time.time() - begin_time))
